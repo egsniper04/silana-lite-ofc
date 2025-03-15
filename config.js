f541956e33edd7935bcb5cc13a0caa681c054b82
egsniper04 <egsniper04@gmail.com>
2025-03-15 04:05:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 
 // Owner
 global.owner = [
-['212717457920', 'Noureddine', true]
+['201020182886', 'Ahmed Abdelfattah', true]
 ]
 global.mods = []
 global.prems = []
